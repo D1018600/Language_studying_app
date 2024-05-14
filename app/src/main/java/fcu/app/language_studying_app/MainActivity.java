5d7f51d3a292c27f4a1a237b72d028c759b0a698
Deanway91 <101314750+Deanway91@users.noreply.github.com>
2024-05-14 21:27:00 +0800

    testing github

--- app/src/main/java/fcu/app/language_studying_app/MainActivity.java
+++ app/src/main/java/fcu/app/language_studying_app/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+  //testing Github
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
