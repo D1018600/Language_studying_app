18ebfd12f1908e8f9a647145db3fe74ea4499cf2
D1018508 <D1018508@o365.fcu.edu.tw>
2024-05-16 17:56:04 +0800

    MainActivity and SelectIdActivity activities designed.

--- app/src/main/java/fcu/app/language_studying_app/MainActivity.java
+++ app/src/main/java/fcu/app/language_studying_app/MainActivity.java
@@ -1,6 +1,9 @@
 package fcu.app.language_studying_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-  //testing Github
+  private Button btnStart;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -21,5 +23,20 @@ public class MainActivity extends AppCompatActivity {
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
       return insets;
     });
+
+    btnStart = findViewById(R.id.btn_start);
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if (view.getId() == R.id.btn_start) {
+          Intent intent = new Intent();
+          intent.setClass(MainActivity.this, SelectIdActivity.class);
+          startActivity(intent);
+        }
+      }
+    };
+
+    btnStart.setOnClickListener(btnListener);
   }
 }
---
+++ app/src/main/java/fcu/app/language_studying_app/SelectIdActivity.java
@@ -0,0 +1,51 @@
+package fcu.app.language_studying_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SelectIdActivity extends AppCompatActivity {
+    private Button btnLearn;
+    private Button btnDesign;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_select_id);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnLearn = findViewById(R.id.btn_learn);
+        btnDesign = findViewById(R.id.btn_design);
+
+        View.OnClickListener btnListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+
+                if (view.getId() == R.id.btn_learn) {
+                    Toast toast = Toast.makeText(SelectIdActivity.this, "Not design yet.", Toast.LENGTH_LONG);
+                    toast.show();
+                } else if (view.getId() == R.id.btn_design) {
+                    intent.setClass(SelectIdActivity.this, DesignStageActivity.class);
+                }
+
+                startActivity(intent);
+            }
+        };
+
+        btnLearn.setOnClickListener(btnListener);
+        btnDesign.setOnClickListener(btnListener);
+    }
+}
