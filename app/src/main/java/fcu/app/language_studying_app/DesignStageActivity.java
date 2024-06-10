f0da5f891498d58f3299eb8f6a7f581573e8db60
CheeseLeaf <jiahua.wang.19@gmail.com>
2024-06-10 17:58:14 +0800

    updated a few return button

--- app/src/main/java/fcu/app/language_studying_app/DesignStageActivity.java
+++ app/src/main/java/fcu/app/language_studying_app/DesignStageActivity.java
@@ -1,6 +1,10 @@
 package fcu.app.language_studying_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DesignStageActivity extends AppCompatActivity {
+    private Button btnReturn;
+    private EditText etTopic;
+    private Button btnAddWord;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,19 @@ public class DesignStageActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnReturn = findViewById(R.id.btn_return_design_stage);
+        etTopic = findViewById(R.id.et_topic);
+        btnAddWord = findViewById(R.id.btn_add_word);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(DesignStageActivity.this, DesignRecordActivity.class);
+                DesignStageActivity.this.startActivity(intent);
+            }
+        };
+        btnReturn.setOnClickListener(listener);
+        btnAddWord.setOnClickListener(listener);
     }
 }
