fe366f60e34c2a1ede40437e4d58aeaa1befaa9a
CheeseLeaf <jiahua.wang.19@gmail.com>
2024-06-15 16:31:10 +0800

    able to upload stage to firebase(no download yet)

--- app/src/main/java/fcu/app/language_studying_app/DesignRecordActivity.java
+++ app/src/main/java/fcu/app/language_studying_app/DesignRecordActivity.java
@@ -2,10 +2,30 @@ package fcu.app.language_studying_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,6 +34,9 @@ import androidx.core.view.WindowInsetsCompat;
 public class DesignRecordActivity extends AppCompatActivity {
     private Button btnReturn;
     private Button btnAddStage;
+    private LinearLayout lvStageContainer;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +51,7 @@ public class DesignRecordActivity extends AppCompatActivity {
 
         btnReturn = findViewById(R.id.btn_return_design_record);
         btnAddStage = findViewById(R.id.btn_add_stage);
+        lvStageContainer = findViewById(R.id.lv_stage_container);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,6 +61,7 @@ public class DesignRecordActivity extends AppCompatActivity {
                 }
                 if(view.getId() == R.id.btn_add_stage){
                     intent.setClass(DesignRecordActivity.this, DesignStageActivity.class);
+
                 }
                 DesignRecordActivity.this.startActivity(intent);
             }
@@ -44,4 +69,19 @@ public class DesignRecordActivity extends AppCompatActivity {
         btnReturn.setOnClickListener(listener);
         btnAddStage.setOnClickListener(listener);
     }
+
+    private void getStages(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("stages");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+
 }
